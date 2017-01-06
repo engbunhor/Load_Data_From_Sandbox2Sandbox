5d34d57400fe021a663d45b8bc696f5d47472535
engbunhor <bunhor2020@mail.com>
2017-01-06 16:39:18 +0700

    rollback prev changes
    delete some echo text
    
    test new commit message
    test add new txt file
    
    adding content to file
    
    test rename
    
    an other update
    
    test 12
    
    test13

--- copyDataFromA2B.bat
+++ copyDataFromA2B.bat
@@ -7,14 +7,14 @@ SET SANDBOX_B_SCRIPTS=%ROOT_PATH%\SandboxB\Scripts
 SET SANDBOX_A_XMLCONF=%ROOT_PATH%\SandboxA\XmlConf
 SET SANDBOX_B_XMLCONF=%ROOT_PATH%\SandboxB\XmlConf
 
-ECHO ============= CLEANING PREVIOUS EXPORTED CSVs =========================--
+ECHO ============= CLEANING PREVIOUS EXPORTED CSVs =========================
 CALL "%SANDBOX_A_SCRIPTS%\cleanPreviousExportedCSVs.bat"
-ECHO ============= CLEANING PREVIOUS EXPORTED CSVs ENDED====================---
+ECHO ============= CLEANING PREVIOUS EXPORTED CSVs ENDED====================
  
 
 ECHO =========================== START EXPORTING ===========================
 CALL "%SANDBOX_A_SCRIPTS%\exportAll.bat"
-ECHO =========================== START EXPORTING ENDED ==============================
+ECHO =========================== START EXPORTING ENDED ========================
 this is from servier
 
 PAUSE
